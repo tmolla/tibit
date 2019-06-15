57300c3e0fd554804b1463f42a53709cb0bc63c3
Telahoun Molla <telahounmolla@gmail.com>
2019-06-15 11:12:02 -0700

    Initial scafolding commit

---
+++ client/build/precache-manifest.a051f0ae7e8cb34c00a5ef1fdfd052c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5bcac3c5148d987b464",
+    "url": "/static/css/main.a06cd8a2.chunk.css"
+  },
+  {
+    "revision": "f5bcac3c5148d987b464",
+    "url": "/static/js/main.3b76859c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "4e10078ba18681866e117a01f79f2c08",
+    "url": "/index.html"
+  }
+];
