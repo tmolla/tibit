3c950a33ec4f0cb4a480c21255ff691e6f28086e
Telahoun Molla <telahounmolla@gmail.com>
2019-07-09 07:01:48 -0700

    Cleanup decleration

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import store from "../../store"
 import "./style.css";
-import authReducer from "../../reducers/authReducer";
 
 class Nav extends Component {
   state = {
